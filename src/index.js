0fe22dc5d97b2003e809c30c053ec70eede9c3de
zhangsan <lxs2048@sina.com>
2023-05-04 19:59:36 +0800

    init

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import React from "react";
+import ReactDOM from "react-dom";
+const rootEle = document.getElementById('didact-root'); // 根据产品名称进行修改 xxx-root
+const element = <h1 title="foo">Hello</h1>
+ReactDOM.render(element, rootEle);
